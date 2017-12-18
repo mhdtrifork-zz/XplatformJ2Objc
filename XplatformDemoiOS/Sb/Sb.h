b11e98012feb10f4bf5a4b6c4da0e842cec83d7b
Morten Hedengran <mhd@trifork.com>
2017-12-18 15:54:32 +0100

    added initial support to iOS

---
+++ XplatformDemoiOS/Sb/Sb.h
@@ -0,0 +1,13 @@
+//
+//  Sb.h
+//  Sb
+//
+//  Created by Morten Hedengran on 18/12/2017.
+//  Copyright © 2017 Trifork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sb : NSObject
+
+@end
---
+++ XplatformDemoiOS/Sb/Sb.m
@@ -0,0 +1,13 @@
+//
+//  Sb.m
+//  Sb
+//
+//  Created by Morten Hedengran on 18/12/2017.
+//  Copyright © 2017 Trifork. All rights reserved.
+//
+
+#import "Sb.h"
+
+@implementation Sb
+
+@end
